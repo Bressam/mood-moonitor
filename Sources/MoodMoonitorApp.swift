1fd7aeeca25ddd2135f45c6f19cab35cce061b9a
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-10-06 18:47:49 -0300

    adds tuist files to generate simple project

---
+++ Sources/MoodMoonitorApp.swift
@@ -0,0 +1,22 @@
+//
+//  MoodMoonitorApp.swift
+//  MoodMoonitor
+//
+//  Created by Giovanne Bressam on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodMoonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+    
+    init() {
+        // Appearance
+//        UITabBar.appearance().unselectedItemTintColor = UIColor.white
+    }
+}
---
+++ Tuist/ProjectDescriptionHelpers/Module.swift
@@ -0,0 +1,36 @@
+import Foundation
+import ProjectDescription
+
+struct Module {
+    let name: String
+    let bundleId: String
+
+    var mainTarget: Target {
+        Target(
+            name: name,
+            sources: "Sources/**",
+            resources: "Resources/**",
+            infoPlist: .extendingDefault(with: [
+                "UILaunchScreen" : ""
+            ]),
+            bundleId: bundleId
+        )
+    }
+
+    struct Target {
+        let name: String
+        let sources: SourceFilesList
+        let resources: ResourceFileElements?
+        let infoPlist: InfoPlist
+        let bundleId: String
+
+        init(name: String, sources: String, resources: String? = nil, infoPlist: InfoPlist? = nil, bundleId: String) {
+            self.name = name
+            self.sources = SourceFilesList(stringLiteral: sources)
+            self.resources = resources == nil ? nil : ResourceFileElements(stringLiteral: resources!)
+            self.infoPlist = infoPlist == nil ? .default : infoPlist!
+            self.bundleId = bundleId
+        }
+    }
+}
+
