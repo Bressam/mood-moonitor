01ab3fb61095b472a4467b6052f61a97768f3033
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-10-07 22:37:50 -0300

    adds signin module with clean architecture

---
+++ Modules/SignIn/Domain/Project.swift
@@ -0,0 +1,10 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: "SignInDomain",
+    bundleIdPrefix: "dev.bressam.mood-moonitor",
+    dependencies: [],
+    disableBundleAccessors: true,
+    disableSynthesizedResourceAccessors: false
+)
---
+++ Modules/SignIn/Presentation/Project.swift
@@ -0,0 +1,10 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: "SignInFeature",
+    bundleIdPrefix: "dev.bressam.mood-moonitor",
+    dependencies: [],
+    disableBundleAccessors: true,
+    disableSynthesizedResourceAccessors: false
+)
