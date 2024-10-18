42f80e4e038e68253a445c9319fa3b04752061a2
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-10-17 23:53:50 -0300

    adds email and password textfields to SignIn view using DesignSystem module

---
+++ Modules/SignIn/Presentation/Implementation/Sources/SignInView.swift
@@ -0,0 +1,42 @@
+//
+//  SignInView.swift
+//  SignIn
+//
+//  Created by Giovanne Bressam on 06/10/24.
+//  Copyright © 2024 dev.bressam. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+public struct SignInView: View {
+    public init() { }
+
+    public var body: some View {
+        VStack(alignment: .center,
+               spacing: SpacingTokens.medium.constant) {
+            MMTextField(fieldTitle: "e-mail",
+                        inputText: "")
+            MMTextField(fieldTitle: "password",
+                        inputText: "",
+                        isSecured: true,
+                        style: .password,
+                        forgotPasswordTitle: "Forgot password?",
+                        forgetPasswordAction: didTapForgetPassword)
+
+            Button {
+                print("Login tapped")
+            } label: {
+                Text("Login")
+            }
+        }.padding([.leading, .trailing], 26)
+    }
+
+    private func didTapForgetPassword() {
+        print("Restore password tapped")
+    }
+}
+
+#Preview {
+    SignInView()
+}
