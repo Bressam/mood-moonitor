a08d4f4a32ef86a1185b7347c1e11e816b3d41db
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-10-17 23:25:17 -0300

    implements design system textfield and also spacing tokens constants

---
+++ Modules/DesignSystem/Components/Implementation/Sources/Components/PrimaryTextField.swift
@@ -0,0 +1,129 @@
+//
+//  MMTextField.swift
+//  DesignSystem
+//
+//  Created by Giovanne Bressam on 17/10/24.
+//  Copyright © 2024 dev.bressam. All rights reserved.
+//
+
+import SwiftUI
+
+enum MMTextFieldStyle {
+    case regular, password
+    
+    var hasSecurityToggle: Bool {
+        switch self {
+        case .regular: false
+        case .password: true
+        }
+    }
+    
+    var hasStorePasswordSwitch: Bool {
+        switch self {
+        case .regular: false
+        case .password: true
+        }
+    }
+    
+    var buttonTitle: String {
+        switch self {
+        case .regular: ""
+        case .password: "Recuperar senha"
+        }
+    }
+}
+
+struct MMTextField: View {
+    @State var fieldTitle: String
+    @State var inputText: String
+    @State var isSecured: Bool = false
+    @State var style: MMTextFieldStyle = .regular
+    var buttonAction: (() -> Void)? = nil
+    @State private var storePassword: Bool = true
+    
+    var body: some View {
+        VStack(alignment: .leading,
+               spacing: SpacingTokens.xsmall.constant) {
+            HStack {
+                Text(fieldTitle)
+                    .font(.callout)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.gray)
+                if let buttonAction {
+                    Spacer()
+                    Button(action: buttonAction, label: {
+                        Text(style.buttonTitle)
+                    })
+                }
+            }
+            
+            HStack {
+                inputField
+                    .padding(.leading, SpacingTokens.small.constant)
+                    .frame(height: 48)
+                Spacer()
+                if style.hasSecurityToggle {
+                    Button(action: {
+                        isSecured.toggle()
+                    }, label: {
+                        if isSecured {
+                            Image(systemName: "eye")
+                                .foregroundStyle(.gray.opacity(0.8))
+                        } else {
+                            Image(systemName: "eye.slash")
+                                .foregroundStyle(.gray.opacity(0.8))
+                        }
+                    }).padding(.trailing, SpacingTokens.small.constant)
+                }
+            }
+            .overlay {
+                RoundedRectangle(cornerRadius: RadiusTokens.small.constant)
+                    .stroke(lineWidth: 2.5)
+            }
+            
+            if style.hasStorePasswordSwitch {
+                storePasswordSwitch
+                    .padding(.top, SpacingTokens.xsmall.constant)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var inputField: some View {
+        if isSecured {
+            SecureField("", text: $inputText)
+                .foregroundStyle(.gray)
+                .font(.caption2)
+                .fontWeight(.ultraLight)
+        } else {
+            TextField("", text: $inputText)
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
+                .font(.callout)
+                .foregroundStyle(.gray)
+        }
+    }
+    
+    private var storePasswordSwitch: some View {
+        HStack {
+            Toggle(isOn: $storePassword, label: {
+                Text("Lembrar minha senha")
+                    .font(.caption)
+            })
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    MMTextField(fieldTitle: "Field title",
+                inputText: "")
+        .padding(24)
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    MMTextField(fieldTitle: "Field title",
+                inputText: "",
+                isSecured: true,
+                style: .password)
+    .padding(24)
+}
