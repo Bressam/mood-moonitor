1f2bcc84f6b587f15eb1bf1d20f572370bc2f2d6
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-10-20 18:34:49 -0300

    adds extension to simplify main project and modules creation with Tuist

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -5,6 +5,7 @@ import Foundation
 import ProjectDescription
 
 extension Project {
+// MARK: - Project
     public static func project(
         name: String,
         appName: String,
@@ -17,35 +18,35 @@ extension Project {
         disableSynthesizedResourceAccessors: Bool = false
     ) -> Project {
         let testPlanFile = "AllTests.xctestplan"
-        let module = Module(name: name, bundleId: bundleId)
+        let application = Application(name: name, bundleId: bundleId)
 
-        // MARK: - Schemes definition
+        // MARK: Schemes definition
         let mainScheme: Scheme = .scheme(
-            name: module.mainTarget.name,
+            name: application.mainTarget.name,
             shared: false,
             buildAction: .buildAction(
-                targets: [.target(module.mainTarget.name)]
+                targets: [.target(application.mainTarget.name)]
             ),
             testAction: .testPlans([
                 .relativeToManifest(testPlanFile)
             ])
         )
 
-        // MARK: - Targets definition
+        // MARK: Targets definition
         var projectTargets = [Target]()
 
-        // MARK: - Main Target
+        // MARK: Main Target
         projectTargets.append(
             .target(
                 name: name,
                 destinations: .iOS,
                 product: .app,
                 productName: appName,
-                bundleId: module.mainTarget.bundleId,
+                bundleId: application.mainTarget.bundleId,
                 deploymentTargets: deploymentTarget,
-                infoPlist: module.mainTarget.infoPlist,
-                sources: module.mainTarget.sources,
-                resources: module.mainTarget.resources,
+                infoPlist: application.mainTarget.infoPlist,
+                sources: application.mainTarget.sources,
+                resources: application.mainTarget.resources,
                 scripts: [.swiftLint],
                 dependencies: dependencies
             )
@@ -66,11 +67,122 @@ extension Project {
             ),
             packages: packages,
             settings: .settings(
-                base: [
-                "ASSETCATALOG_COMPILER_GENERATE_SWIFT_ASSET_SYMBOL_EXTENSIONS": "YES",
-                "ENABLE_USER_SCRIPT_SANDBOXING": "YES"
-                ]),
+                base: .baseSettings
+            ),
             targets: projectTargets,
+            schemes: [mainScheme],
+            resourceSynthesizers: [.assets()]
+        )
+    }
+  
+// MARK: - Module
+    public static func module(
+        name: String,
+        hasInterface: Bool = true,
+        hasTests: Bool = true,
+        organizationName: String = "dev.bressam",
+        bundleIdPrefix: String,
+        deploymentTarget: DeploymentTargets = .iOS("17.0"),
+        implementationDependencies: [ProjectDescription.TargetDependency] = [],
+        interfaceDependencies: [ProjectDescription.TargetDependency] = [],
+        testDependencies: [ProjectDescription.TargetDependency] = [],
+        packages: [ProjectDescription.Package] = [],
+        disableBundleAccessors: Bool = true,
+        disableSynthesizedResourceAccessors: Bool = false
+    ) -> Project {
+        let testPlanFile = "AllTests.xctestplan"
+        let module = Module(name: name,
+                            bundleId: bundleIdPrefix + "." + name)
+        
+        // MARK: Schemes definition
+        let mainScheme: Scheme = .scheme(
+            name: module.mainTarget.name,
+            shared: false,
+            buildAction: .buildAction(
+                targets: [.target(module.mainTarget.name)]
+            )
+        )
+        
+        // MARK: Targets definition
+        var moduleTargets = [Target]()
+        
+        // MARK: Interface
+        if hasInterface {
+            moduleTargets.append(
+                .target(
+                    name: module.interfaceTarget.name,
+                    destinations: .iOS,
+                    product: .framework,
+                    productName: module.interfaceTarget.name,
+                    bundleId: module.interfaceTarget.bundleId,
+                    deploymentTargets: deploymentTarget,
+                    infoPlist: module.interfaceTarget.infoPlist,
+                    sources: module.interfaceTarget.sources,
+                    resources: module.interfaceTarget.resources,
+                    scripts: [.swiftLint],
+                    dependencies: interfaceDependencies
+                )
+            )
+        }
+        
+        // MARK: Main Target
+        let interfaceTarget: [ProjectDescription.TargetDependency] = hasInterface ? [ .target(name: module.interfaceTarget.name) ] : []
+    
+        moduleTargets.append(
+            .target(
+                name: module.mainTarget.name,
+                destinations: .iOS,
+                product: .framework,
+                productName: module.mainTarget.name,
+                bundleId: module.mainTarget.bundleId,
+                deploymentTargets: deploymentTarget,
+                infoPlist: module.mainTarget.infoPlist,
+                sources: module.mainTarget.sources,
+                resources: module.mainTarget.resources,
+                scripts: [.swiftLint],
+                dependencies: implementationDependencies + interfaceTarget
+            )
+        )
+        
+        // MARK: Test Target
+        if hasTests {
+            moduleTargets.append(
+                .target(
+                    name: module.testTarget.name,
+                    destinations: .iOS,
+                    product: .unitTests,
+                    productName: module.testTarget.name,
+                    bundleId: module.testTarget.bundleId,
+                    deploymentTargets: deploymentTarget,
+                    infoPlist: module.testTarget.infoPlist,
+                    sources: module.testTarget.sources,
+                    resources: module.testTarget.resources,
+                    scripts: [.swiftLint],
+                    dependencies: testDependencies + interfaceTarget + [
+                        .target(name: module.mainTarget.name)
+                    ]
+                )
+            )
+        }
+
+        return Project(
+            name: name,
+            organizationName: organizationName,
+            options: .options(
+                automaticSchemesOptions: .enabled(
+                    targetSchemesGrouping: .notGrouped,
+                    codeCoverageEnabled: true,
+                    testingOptions: [],
+                    testScreenCaptureFormat: .screenshots
+                ),
+                disableBundleAccessors: disableBundleAccessors,
+                disableSynthesizedResourceAccessors: disableSynthesizedResourceAccessors
+            ),
+            packages: packages,
+            settings: .settings(
+                base: .moduleSettings
+            ),
+            targets: moduleTargets,
             schemes: [mainScheme],
             resourceSynthesizers: [.assets()]
         )
