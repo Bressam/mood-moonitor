1f2c9ee28c8fa616f46c3150218ea2d49a2cd4b1
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-10-26 18:54:57 -0300

    RegisterMood: adds MoodsRepository and MoodsUseCase

---
+++ Modules/RegisterMood/Domain/Interface/Sources/Entities/Mood.swift
@@ -0,0 +1,11 @@
+//
+//  Mood.swift
+//  RegisterMoodDomain
+//
+//  Created by Giovanne Bressam on 26/10/24.
+//  Copyright © 2024 dev.bressam. All rights reserved.
+//
+
+public struct Mood: Codable {
+    public var name: String
+}
---
+++ Modules/RegisterMood/Domain/Interface/Sources/UseCases/RetrieveAvailableMoodsUseCaseProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  RetrieveAvailableMoodsUseCaseProtocol.swift
+//  RegisterMoodDomain
+//
+//  Created by Giovanne Bressam on 13/10/24.
+//  Copyright © 2024 dev.bressam. All rights reserved.
+//
+
+public protocol RetrieveAvailableMoodsUseCaseProtocol: AnyObject {
+    func execute() async throws -> [Mood]
+}
